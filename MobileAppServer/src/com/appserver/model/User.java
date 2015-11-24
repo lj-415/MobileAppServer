5fd138c473a96fa91b97985a60e55027cecd2f4d
lj415 <80481613@qq.com>
2015-11-24 09:28:49 +0800

    增加用户属性

--- MobileAppServer/src/com/appserver/model/User.java
+++ MobileAppServer/src/com/appserver/model/User.java
@@ -7,14 +7,23 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 6036028588379359712L;
 	
 	private int id;
+	private String userId;
 	private String userName;
 	private String password;
+	private String sex;
+	private String telephone;
 	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 	public String getUserName() {
 		return userName;
@@ -28,5 +37,17 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
 }
